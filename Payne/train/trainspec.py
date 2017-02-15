52f9baea79b9f2a759833489bae16adb519b95c8
pacargile <pcargile@cfa.harvard.edu>
2017-02-15 11:34:31 -0500

    save HDF5 iter

--- Payne/train/trainspec.py
+++ Payne/train/trainspec.py
@@ -162,30 +162,22 @@ class TrainSpec(object):
 			# map out the pixel training
 			# pool.map(self,range(numtrainedpixles))
 			netout = pool.imap(self,range(numtrainedpixles))
-			for ii,net in enumerate(netout):
-				sys.stdout.flush()
-				# store and flush the network parameters into the HDF5 file
-				w0_h5[ii,...] = net.layers[0].w.get_value().T
-				b0_h5[ii,...] = net.layers[0].b.get_value()
-				w1_h5[ii,...] = net.layers[1].w.get_value()[:,0]
-				b1_h5[ii,...] = net.layers[1].b.get_value()[0]
-
-				# flush the HDF5 file to store the output
-				outfile.flush()
 
 		else:
 			# map out the pixel training
 			netout = imap(self,range(numtrainedpixles))
-			for ii,net in enumerate(netout):
-				sys.stdout.flush()
-				# store and flush the network parameters into the HDF5 file
-				w0_h5[ii,...] = net.layers[0].w.get_value().T
-				b0_h5[ii,...] = net.layers[0].b.get_value()
-				w1_h5[ii,...] = net.layers[1].w.get_value()[:,0]
-				b1_h5[ii,...] = net.layers[1].b.get_value()[0]
 
-				# flush the HDF5 file to store the output
-				outfile.flush()
+		for ii,net in enumerate(netout):
+			sys.stdout.flush()
+			# store and flush the network parameters into the HDF5 file
+			w0_h5[ii,...] = net.layers[0].w.get_value().T
+			b0_h5[ii,...] = net.layers[0].b.get_value()
+			w1_h5[ii,...] = net.layers[1].w.get_value()[:,0]
+			b1_h5[ii,...] = net.layers[1].b.get_value()[0]
+
+			# flush the HDF5 file to store the output
+			outfile.flush()
+
 
 		# start total timer
 		tottimestart = datetime.now()
