50bb7a0b44c66b628e51bd735654c07cfc2cae6e
pacargile <pcargile@cfa.harvard.edu>
2022-03-29 16:30:45 -0400

    improving diagnostic plots

--- Payne/testing/testspec.py
+++ Payne/testing/testspec.py
@@ -35,8 +35,8 @@ class TestSpec(object):
 		# initialize the Payne Predictor
 		self.NN = predictspec.ANN(nnpath=self.NNfilename,NNtype=self.nntype,verbose=False)
 
-		# define wavelength range of trained network
-		self.waverange = [self.NN['wavelength'].min(),self.NN['wavelength'].max()]
+		# define wavelengths
+		self.wave = self.NN['wavelength'][:]
 
 		# default resolution is the native resolution of NN
 		self.resolution = float(np.array(self.NN['resolution']))
@@ -46,6 +46,9 @@ class TestSpec(object):
 		'''
 		Function to run the testing on an already trained network
 		'''
+		# output plot file
+		output = kwargs.get('output','./test.pdf')
+
 		if 'testnum' in kwargs:
 			testnum = kwargs['testnum']
 			ind = rng.integers(low=0,high=len(self.NN['testlabels']),size=testnum)
@@ -61,7 +64,8 @@ class TestSpec(object):
 		# make NN predictions for all test labels
 		nnpred = np.array([NN.eval(pars) for pars in testlabels])
 
-		output = kwargs.get('output','./test.pdf')
+		# make residual array
+		modres = np.array([np.abs(x-y) for x,y in zip(testpred,nnpred)])
 
 		# initialize PDF file
 		with PdfPages(output) as pdf:
@@ -69,13 +73,63 @@ class TestSpec(object):
 			# MAD histograms binned by pars
 			fig,ax = plt.subplots(nrows=2,ncols=2,constrained_layout=True)
 
-			
+			# teff binned
+			ind = testlabels['teff'] > 7000.0
+			ax[0,0].hist(np.median(modres[ind]),bins=25,c='C0')
+			ind = (testlabels['teff'] <= 7000.0) & (testlabels['teff'] > 6000.0)
+			ax[0,0].hist(np.median(modres[ind]),bins=25,c='C3')
+			ind = (testlabels['teff'] <= 5500.0) & (testlabels['teff'] > 4500.0)
+			ax[0,0].hist(np.median(modres[ind]),bins=25,c='C4')
+			ind = (testlabels['teff'] <= 4500.0) 
+			ax[0,0].hist(np.median(modres[ind]),bins=25,c='C5')
+
+			# logg binned
+			ind = testlabels['logg'] > 4.5
+			ax[0,1].hist(np.median(modres[ind]),bins=25,c='C0')
+			ind = (testlabels['logg'] <= 4.5) & (testlabels['logg'] > 4.0)
+			ax[0,1].hist(np.median(modres[ind]),bins=25,c='C3')
+			ind = (testlabels['logg'] <= 4.0) & (testlabels['logg'] > 3.5)
+			ax[0,1].hist(np.median(modres[ind]),bins=25,c='C4')
+			ind = (testlabels['logg'] <= 3.5) 
+			ax[0,1].hist(np.median(modres[ind]),bins=25,c='C5')
+
+			# feh binned
+			ind = testlabels['feh'] > 0.25
+			ax[1,0].hist(np.median(modres[ind]),bins=25,c='C0')
+			ind = (testlabels['feh'] <= 0.25) & (testlabels['feh'] > -0.5)
+			ax[1,0].hist(np.median(modres[ind]),bins=25,c='C3')
+			ind = (testlabels['feh'] <= -0.5) & (testlabels['feh'] > -1.5)
+			ax[1,0].hist(np.median(modres[ind]),bins=25,c='C4')
+			ind = (testlabels['feh'] <= -1.5) 
+			ax[1,0].hist(np.median(modres[ind]),bins=25,c='C5')
+
+			# afe binned
+			ind = testlabels['afe'] > 0.4
+			ax[1,1].hist(np.median(modres[ind]),bins=25,c='C0')
+			ind = (testlabels['afe'] <= 0.4) & (testlabels['afe'] > 0.2)
+			ax[1,1].hist(np.median(modres[ind]),bins=25,c='C3')
+			ind = (testlabels['afe'] <= 0.2) & (testlabels['afe'] > 0.0)
+			ax[1,1].hist(np.median(modres[ind]),bins=25,c='C4')
+			ind = (testlabels['afe'] <= 0.0) 
+			ax[1,1].hist(np.median(modres[ind]),bins=25,c='C5')
+
+			ax[1,0].set_xlabel('median MAD per spectrum')
+			ax[1,1].set_xlabel('median MAD per spectrum')
 
 			pdf.savefig(fig)
 			plt.close(fig)
 
 			# build MAD versus lambda plots
 			fig,ax = plt.subplots(nrows=2,ncols=1,constrained_layout=True)
+
+			ax[0].scatter(self,wave,np.median(modres,axis=0),marker='o')
+			ax[1].hist(np.median(modres,axis=0),bins=25,cumulative=True,density=True)
+
+			ax[0].set_xlabel(r'$\lambda')
+			ax[0].set_xlabel('median MAD @ pixel')
+
+			ax[1].set_xlabel('MAD')
+			ax[1].set_ylabel('CDF')
 
 			pdf.savefig(fig)
 			plt.close(fig)
