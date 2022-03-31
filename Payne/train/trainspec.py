508e8a991e2d231a1d67c65c25e6a69e768abe99
pacargile <pcargile@cfa.harvard.edu>
2022-03-31 17:39:54 -0400

    changed loss function to MSE

--- Payne/train/trainspec.py
+++ Payne/train/trainspec.py
@@ -310,10 +310,10 @@ class TrainMod(object):
           model.train()
 
           # initialize the loss function
-          # loss_fn = torch.nn.MSELoss(reduction='mean')
+          loss_fn = torch.nn.MSELoss(reduction='sum')
           # loss_fn = torch.nn.SmoothL1Loss(reduction='mean')
           # loss_fn = torch.nn.KLDivLoss(size_average=False)
-          loss_fn = torch.nn.L1Loss(reduction = 'sum')
+          # loss_fn = torch.nn.L1Loss(reduction = 'sum')
 
           # initialize the optimizer
           learning_rate = self.lr
