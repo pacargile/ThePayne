74db6f54188860ed239734b75fac85474aa1cd5b
Phillip Cargile <pcargile@cfa.harvard.edu>
2022-04-12 07:58:39 -0400

    depreciated index_update call

--- Payne/jax/smoothing.py
+++ Payne/jax/smoothing.py
@@ -6,7 +6,7 @@
 import jax.numpy as np
 import numpy as nnp
 from jax.numpy.fft import fft, rfft, irfft, ifft, fftfreq, rfftfreq
-from jax.ops import index, index_add, index_update
+# from jax.ops import index, index_add, index_update
 # from scipy.special import j1
 from .jaxj1 import j1
 from jax import jit,vmap,lax
@@ -549,7 +549,8 @@ def smooth_fft(dx, spec, sigma):
     # Make the fourier space taper; just the analytical fft of a gaussian
     taper = np.exp(-2 * (np.pi ** 2) * (sigma ** 2) * (ss ** 2))
     # ss[0] = 0.01  # hack
-    ss = index_update(ss, index[0], 0.01)
+    # ss = index_update(ss, index[0], 0.01)
+    ss = ss.at[0].set(0.01)
     # Fourier transform the spectrum
     spec_ff = jrfft(spec)
     # Multiply in fourier space
@@ -564,13 +565,13 @@ def smooth_fft_vsini(dv,spec,sigma):
 
     # Make the fourier space taper
     # ss[0] = 0.01 #junk so we don't get a divide by zero error
-    # ss.at[0].set(0.01)
-    ss = index_update(ss, index[0], 0.01)
+    # ss = index_update(ss, index[0], 0.01)
+    ss = ss.at[0].set(0.01)
     ub = 2. * np.pi * sigma * ss
     sb = jj1(ub) / ub - 3 * np.cos(ub) / (2 * ub ** 2) + 3. * np.sin(ub) / (2 * ub ** 3)
     #set zeroth frequency to 1 separately (DC term)
-    sb = index_update(sb, index[0], 1.0)
-    # sb.at[0].set(1.0)
+    # sb = index_update(sb, index[0], 1.0)
+    sb = sb.at[0].set(1.0)
 
     # Fourier transform the spectrum
     FF = jrfft(spec)
