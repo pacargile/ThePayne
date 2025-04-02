eecf2916b6f98c7368dfac22263e42790b6f0a4c
pacargile <pcargile@cfa.harvard.edu>
2025-04-02 09:41:55 -0400

    trying out new model/training scheme

--- Payne/train/NNmodels_new.py
+++ Payne/train/NNmodels_new.py
@@ -3,29 +3,10 @@ from collections import OrderedDict
 
 
 # linear feed-foward model with simple activation functions
-class MLP(nn.Module):  
+class MLP_v0(nn.Module):  
     def __init__(self, D_in, H1, H2, H3, D_out):
-        super(MLP, self).__init__()
+        super(MLP_v0, self).__init__()
 
-        # self.mlp = nn.Sequential(OrderedDict([
-        #     ('lin1',nn.Linear(D_in, H1)),
-        #     ('af1',nn.GELU()),
-        #     ('lin2',nn.Linear(H1,H1)),
-        #     ('af2',nn.GELU()),
-        #     ('lin3',nn.Linear(H1,H2)),
-        #     ('af3',nn.GELU()),
-        #     ('lin4',nn.Linear(H2,H2)),
-        #     ('af4',nn.GELU()),
-        #     ('lin5',nn.Linear(H2,H2)),
-        #     ('af5',nn.GELU()),
-        #     ('lin6',nn.Linear(H2,H2)),
-        #     ('af6',nn.GELU()),
-        #     ('lin7',nn.Linear(H2,H2)),
-        #     ('af7',nn.GELU()),
-        #     ('lin8',nn.Linear(H2,H3)),
-        #     ('af8',nn.GELU()),
-        #     ('lin9',nn.Linear(H3,D_out)), 
-        # ]))
 
         self.mlp = nn.Sequential(OrderedDict([
             ('lin1',nn.Linear(D_in, H1)),
@@ -37,7 +18,7 @@ class MLP(nn.Module):
             ('lin3',nn.Linear(H2, H3)),
             ('ln3',nn.LayerNorm(H3)),
             ('af3',nn.SiLU()),
-            ('d1',nn.Dropout(0.2)),
+            ('d1',nn.Dropout(0.3)),
             ('lin4',nn.Linear(H3, H3)),
             ('ln4',nn.LayerNorm(H3)),
             ('af4',nn.SiLU()),
@@ -47,8 +28,30 @@ class MLP(nn.Module):
             ('lin6', nn.Linear(H3, D_out))
         ]))
 
+    def forward(self, x):
+        y_i = self.mlp(x)
+        return y_i     
+    
+    
+# linear feed-foward model with simple activation functions
+class MLP_v1(nn.Module):  
+    def __init__(self, D_in, H1, H2, H3, D_out):
+        super(MLP_v1, self).__init__()
+
+        self.mlp = nn.Sequential(OrderedDict([
+            ('lin1',nn.Linear(D_in, H1)),
+            ('ln1',nn.LayerNorm(H1)),
+            ('af1',nn.SiLU()),  
+            ('lin2',nn.Linear(H1, H2)),
+            ('ln2',nn.LayerNorm(H2)),
+            ('af2',nn.SiLU()),
+            ('d1',nn.Dropout(0.4)),
+            ('lin3',nn.Linear(H2, H3)),
+            ('ln3',nn.LayerNorm(H3)),
+            ('af3',nn.SiLU()),
+            ('linout', nn.Linear(H3, D_out))
+        ]))
 
     def forward(self, x):
-#         x_i = self.encode(x)
         y_i = self.mlp(x)
         return y_i     
